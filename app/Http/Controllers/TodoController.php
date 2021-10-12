039da6a72f77a276165e7cb71c5612622534c158
Anowar <1hafizajr@gmail.com>
2021-10-12 21:49:24 +0600

    adding item

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,89 +1,46 @@
 <?php
-
 namespace App\Http\Controllers;
-
+use App\Http\Requests\TodoRequest;
 use App\Models\Todo;
-use Illuminate\Http\Request;
+ 
 
-class TodoController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-       return view('todos.index');
-    }
+class TodoController extends Controller{
+    
+    public function index(){
+        $todos = Todo::all();
+         return view('todos.index')->with(['todos' => $todos]);
+}
+ 
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
+    public function create(){
         return view('todos.create');
 
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request){
-        $request->validate([
-            'title' => 'required|max:200',
-        ]);
+   
+    public function store(TodoRequest $request){
         Todo::create($request->all());
         return redirect()->back()->with('message', 'Todo Created Successfully');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
+   
     public function show(Todo $todo)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit(Todo $todo){
         return view('todos.edit');
       
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Todo $todo)
-    {
+ 
+    public function update(Request $request, Todo $todo){
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Todo $todo)
-    {
+   
+    public function destroy(Todo $todo){
         //
     }
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,1 +1,9 @@
-index
+@extends('layouts.app')
+
+@section('content')
+
+@foreach ($todos as $todo)
+<h2>{{$todo->title}}</h2>
+@endforeach
+
+@endsection
